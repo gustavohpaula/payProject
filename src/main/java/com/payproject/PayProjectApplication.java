83c2af77020c0bca206708619c23522a786cd3bf
gustavohpaula <gustavohp4@gmail.com>
2024-02-17 15:34:51 -0300

    feat: criando o projeto

---
+++ src/main/java/com/payproject/PayProjectApplication.java
@@ -0,0 +1,13 @@
+package com.payproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/payproject/payproject/PayProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payproject.payproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
