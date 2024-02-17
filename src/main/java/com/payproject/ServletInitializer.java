1061747ae4f8b2fcd589ea2042ebc2c1425f5439
gustavohpaula <gustavohp4@gmail.com>
2024-02-17 15:35:12 -0300

    feat: criando o projeto

---
+++ src/main/java/com/payproject/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.payproject;
+
+import com.payproject.PayProjectApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PayProjectApplication.class);
+	}
+
+}
